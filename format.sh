b20d4b006d86054ae0c5edc9dfbe736b59d036d4
Heaven Volkoff <vasconcellos.dev@gmail.com>
2019-08-27 14:53:44 -0300

    Fix process_sh missing shfmt checking Correctly identify empty files Some minor changes

--- format.sh
+++ format.sh
@@ -11,15 +11,16 @@ set -o errtrace # trace ERR through 'time command' and other functions
 # set -n # Check script syntax, without execution.
 
 # ----- Read only properties ---------------------------------------------------
-__pwd="$(pwd)"
-__dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
-__file="${__dir}/$(basename -- "$0")"
-__base="$(basename ${__file} .sh)"
-__root="$(cd "$(dirname "${__dir}")" && pwd)"
+readonly __pwd="$(pwd)"
+readonly __dir=$(CDPATH="" cd -- "$(dirname -- "$0")" && pwd)
+readonly __file="${__dir}/$(basename -- "$0")"
+readonly __base="$(basename "${__file}" .sh)"
+readonly __root="$(cd "$(dirname "${__dir}")" && pwd)"
 __return=""
 
 read_config() {
     if [ -f "./.editorconfig" ]; then
+        # shellcheck disable=SC2005
         echo "$("$__dir/read_config.py" "./.editorconfig" "$1" "$2")"
     else
         echo ""
@@ -27,12 +28,14 @@ read_config() {
 }
 
 process_py() {
+    local import_sorted, black_config
+
     if ! { hash isort && hash black; } 2>/dev/null; then
         echo >&2 "Python: isort or black is not installed"
         return 1
     fi
 
-    local import_sorted="$(isort -q -ac "$1" -d)"
+    import_sorted="$(isort -q -ac "$1" -d)"
     if [ -z "$import_sorted" ]; then
         import_sorted="$(cat "$1")"
     else
@@ -42,7 +45,7 @@ process_py() {
         fi
     fi
 
-    local black_config="$(read_config "**.py" black_file)"
+    black_config="$(read_config "**.py" black_file)"
     if [ -n "$black_config" ]; then
         black_config="--config $black_config"
     fi
@@ -53,42 +56,44 @@ process_py() {
 }
 
 process_sh() {
+    local language_variant, indent_size, binary_start, ident_case, space_redirect, keep_padding, minify
+
     if ! hash shfmt; then
         echo >&2 "Bash: shfmt is not installed"
-        exit 1echo "$formatted"echo "$formatted"
+        exit 1
     fi
 
-    local language_variant="$(read_config "**.sh" language_variant)"
+    language_variant="$(read_config "**.sh" language_variant)"
     if [ -n "$language_variant" ]; then
         language_variant="-ln $language_variant"
     fi
 
-    local indent_size="$(read_config "**.sh" indent_size)"
+    indent_size="$(read_config "**.sh" indent_size)"
     if [ -n "$indent_size" ]; then
         indent_size="-i $indent_size"
     fi
 
-    local binary_start="$(read_config "**.sh" binary_start)"
+    binary_start="$(read_config "**.sh" binary_start)"
     if [ "$binary_start" == "true" ]; then
         binary_start="-bn"
     fi
 
-    local ident_case="$(read_config "**.sh" ident_case)"
+    ident_case="$(read_config "**.sh" ident_case)"
     if [ "$ident_case" == "true" ]; then
         ident_case="-ci"
     fi
 
-    local space_redirect="$(read_config "**.sh" space_redirect)"
+    space_redirect="$(read_config "**.sh" space_redirect)"
     if [ "$space_redirect" == "true" ]; then
         space_redirect="-sr"
     fi
 
-    local keep_padding="$(read_config "**.sh" keep_padding)"
+    keep_padding="$(read_config "**.sh" keep_padding)"
     if [ "$keep_padding" == "true" ]; then
         keep_padding="-kp"
     fi
 
-    local minify="$(read_config "**.sh" minify)"
+    minify="$(read_config "**.sh" minify)"
     if [ "$minify" == "true" ]; then
         minify="-mn"
     fi
@@ -100,7 +105,7 @@ process_sh() {
 
 if [ "$#" -lt "1" ]; then
     echo >&2 "Missing argument"
-    exit -1
+    exit 1
 fi
 
 for file in "$@"; do
@@ -114,12 +119,15 @@ for file in "$@"; do
     file_ext="${filename##*.}"
 
     process="process_${file_ext}"
-    if ! type "$process" 2>&1 1>/dev/null; then
+    if ! type "$process" 1>/dev/null 2>&1; then
         echo >&2 "$file: Doesn't have a recognizable extension"
         continue
     fi
 
-    if $process "$file" && [ -n "$__return" ]; then
+    if [ -z "$(cat "$file")" ]; then
+        echo >&2 "$file: Is empty"
+        continue
+    elif $process "$file" && [ -n "$__return" ]; then
         formatted="$__return"
     else
         echo >&2 "$file: Failed to formatted"
